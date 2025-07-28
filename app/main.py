cdc6178483018e4d86854be8f29f731c20141b8c
mehwash shahzadi <mehwashshahzadi@gmail.com>
2025-07-28 14:33:51 +0800

    add commit

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class Student(BaseModel):
+    """Data model for a student."""
     name: str
     rollno: int
 
@@ -17,32 +18,35 @@ students=[
         "rollno":26
     }
 ]
-#READ: GET THE LIST OF ALL STUDENT
+
 @app.get("/students")
 def getTodo():
+    """Retrieve the list of all students."""
     return students
 
-#CREATE: ADD NEW STUDENTS
 @app.post("/addStudent")
 def addStudent(student:Student): # its a type hint . Accepts and validates JSON input using Pydantic
+    """Add a new student to the list.Raises 400 if the roll number already exists."""
     for s in students:
         if s["rollno"] == student.rollno:
                         raise HTTPException(status_code=400, detail="Roll number already exists")
     students.append(student.dict())
     return {"message": "Student added successfully", "students": students}
 
-# DELETE A STUDENT BY ROLL NUMBER
+
 @app.delete("/students/{rollno}")
 def delete_student(rollno: int):
+    """ Delete a student by roll number.Raises 404 if the student is not found."""
     for s in students:
         if s["rollno"] == rollno:
             students.remove(s)
             return {"message": "Student deleted", "students": students}
     raise HTTPException(status_code=404, detail="Student not found")
 
-# UPDATE STUDENT DETAIL BY ROLL NUMBER
+#
 @app.put("/students/{rollno}")
 def update_student(rollno: int, updated_student: Student):
+    """Update a student's information by roll number.Raises 404 if the student is not found."""
     for index, s in enumerate(students):
         if s["rollno"] == rollno:
             students[index] = updated_student.dict()
